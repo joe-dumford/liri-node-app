2104092d8d5820db8dc4459fb22b85f486853ed6
Joe Dumford <dumford.joe@gmail.com>
2019-04-11 17:17:37 -0400

    Liri Bot set up. adding screenshots and gifs later

---
+++ assets/keys.js
@@ -0,0 +1,13 @@
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+exports.bandsInTown = {
+  key: process.env.BAND_KEY
+};
+exports.movie = {
+  key: process.env.OMDB_KEY
+};
+
+console.log('this is loaded');
+
